77bbed8a59bf9efea962c34db5f964049d364e05
DarwinsOmondi <darwinsomondi05@gmail.com>
2024-06-13 22:05:20 +0300

    ElectricBillManager

--- ElectricalBillManager.c
+++ ElectricalBillManager.c
@@ -27,38 +27,22 @@ int main() {
         Bill = UnitConsumed * 1.20;
     } else if (UnitConsumed >= 200 && UnitConsumed < 400) {
         Bill = UnitConsumed * 1.50;
-            if (Bill < 400) {
-        extra = Bill * percentage;
-        TotalPay = Bill + extra;
-    } else {
-        TotalPay = Bill;
-    }
     } else if (UnitConsumed >= 400 && UnitConsumed < 600) {
         Bill = UnitConsumed * 1.80;
-            if (Bill < 400) {
-        extra = Bill * percentage;
-        TotalPay = Bill + extra;
-    } else {
-        TotalPay = Bill;
-    }
     } else {
         Bill = UnitConsumed * 2.00;
-            if (Bill < 400) {
+    }
+
+    if (Bill < 100) {
+        TotalPay = 100;
+    } else if (Bill > 400) {
         extra = Bill * percentage;
         TotalPay = Bill + extra;
     } else {
         TotalPay = Bill;
     }
-    }
-
-    // if (Bill < 400) {
-    //     extra = Bill * percentage;
-    //     TotalPay = Bill + extra;
-    // } else {
-    //     TotalPay = Bill;
-    // }
 
     printf("%s, your bill is Ksh %.2f\n", CustomerName, TotalPay);
-
+    
     return 0;
 }
